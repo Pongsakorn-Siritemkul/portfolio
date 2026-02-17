f408f78218d99e73730ed9aa1046dc48bcf99014
Pongsakorn-Siritemkul <s6703052411295@email.kmutnb.ac.th>
2026-02-17 13:42:14 +0700

    fix educationCard

--- webwork/src/components/EducationCard.js
+++ webwork/src/components/EducationCard.js
@@ -2,22 +2,37 @@ import React from "react";
 
 const EducationCard = () => {
   return (
-    <div className="card" style={{ padding: '24px' }}>
-      <h2 style={{ fontSize: '20px', margin: '0 0 16px 0' }}>Education</h2>
-        <div style={{ display: 'flex', gap: '16px' }}>
-            <div style={{
-                fontSize: '24px',
-                background: '#f1f5f9',
-                padding: '12px',
-                borderRadius: '12px',
-                height: 'fit-content'           
-            }}>🎓
-            </div> 
-            <div></div>
-                <h3 style={{ margin: 0, fontSize: '18px' }}>King Mongkut's University of Technology North Bangkok</h3>
-                <p style={{ margin: 0, color: 'var(--text-muted)', fontSize: '15px' }}>Bachelor of Industrial Technology in Electronic Technology</p>
+    <div>
+        <div className="card" style={{ padding: '24px' }}>
+        <h2 style={{ fontSize: '20px', margin: '0 0 16px 0' }}>Education</h2>
+            <div style={{ display: 'flex', gap: '24px' , marginBottom: '24px' }}>
+                <div style={{
+                    fontSize: '24px',
+                    background: '#f1f5f9',
+                    padding: '12px',
+                    borderRadius: '12px',
+                    height: 'fit-content'           
+                }}>🎓
+                </div> 
+                <h3 style={{ margin: 0, fontSize: '18px' }}>Loei Technical College</h3>
+                <p style={{ margin: 0, color: 'var(--text-muted)', fontSize: '15px' }}>Diploma in Computer Technology</p>
+                <h3 style={{ margin: 0, fontSize: '18px' }}>GPAX 3.88</h3>
+                <p style={{ margin: 0, color: '#94a3b8' }}>2019 - 2021</p>
+            </div>
+            <div style={{ display: 'flex', gap: '24px' }}>
+                <div style={{
+                    fontSize: '24px',
+                    background: '#f1f5f9',
+                    padding: '12px',
+                    borderRadius: '12px',
+                    height: 'fit-content'           
+                }}>🎓
+                </div> 
+                <h3 style={{ margin: 0, fontSize: '13px' }}>King Mongkut's University <br></br>of Technology North Bangkok</h3>
+                <p style={{ margin: 0, color: 'var(--text-muted)', fontSize: '15px' }}>Bachelor of Industrial Technology</p>
                 <h3 style={{ margin: 0, fontSize: '18px' }}>GPAX 3.31</h3>
-                <p style={{ margin: 0, color: '#94a3b8' }}>2022 - Present</p>
+                <p style={{ margin: 0, color: '#94a3b8' }}>2022 - present</p>
+            </div>
         </div>
     </div>
   );
